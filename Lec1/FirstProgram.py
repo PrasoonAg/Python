49972f1d85ad7ec3afac86e9f6bf9875518d3734
Prasoon Agrawal <prasoonagrawal51360@gmail.com>
2025-10-28 00:41:05 +0530

    Learned something

--- Lec1/FirstProgram.py
+++ Lec1/FirstProgram.py
@@ -1,1 +1,33 @@
+#FIRST PROGRAM IN PYTHON
 print("Hello World!!")
+print("Hey me, don't be just any programmer, be a an awesome one!!") 
+print()
+
+#ARTHEMATIC OPERATIONS
+print (5+6)
+print (23)
+print(2*2) 
+print()
+
+#VARIABLES AND STRINGS
+name = "Prasoon"  #string variable
+age = 99          #integer variable
+price = -9.999     #float variable
+
+print("My name is : ", name)
+print("My age is : ", age)
+print ("the price of a pen is :", price)
+print()
+
+#DATA TYPES
+print(type(name))
+print(type(age))
+print(type(price))
+print()
+
+#USING VARIABLES
+age1 = 20
+age2 = age1
+print("age1 is :", age1)
+print("age2 is :", age2)
+print()
