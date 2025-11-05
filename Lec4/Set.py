1f5c26fa8ca983a2125a180425afd57ea3c11fce
Prasoon Agrawal <prasoonagrawal51360@gmail.com>
2025-11-05 17:09:47 +0530

    5/11/2025

---
+++ Lec4/Set.py
@@ -0,0 +1,29 @@
+#04
+#=========================================================================#
+# Set in Python => Each vaule should be unique.
+#=========================================================================#
+collection = {1,2,3,4,"hello","world"}
+#only values are stored # Set property = Unordered + Unqiue Elements
+
+#The Python set uses a hashing mechanism internally to store elements.
+
+# Numbers like 1, 2, 3, 4 have simple hash values, so they often appear in ascending order by chance.
+
+# Strings like "hello" and "world" have different hash values, so their order depends on the internal hashing — it looks “random.”
+
+print (collection)
+print(type(collection)) # <class 'set'>
+#=========================================================================#
+collection2= {1,2,2,2,"world", " hello" , "world" , 4}
+# Duplicate vaules will be printed once.
+print(collection2)
+print(len(collection2)) #Duplicate vaules will be counted only once.
+#=========================================================================#
+#Empty Set
+null_Dicationary={}
+print(type(null_Dicationary))
+#This is creating the dicationary not a Set.
+
+null_Set = set() #empty set; syntax
+print(type(null_Set))
+
---
+++ Lec4/Set_Methods.py
@@ -0,0 +1,46 @@
+#05
+#=========================================================================#
+# Set Methods
+#=========================================================================#
+collection = set() #Empty/Null set
+
+# add
+collection.add(1)
+collection.add(2)
+collection.add(2) #value will be considered one time.
+collection.add(3)
+collection.add("Prasoon") # added String (immutable)
+collection.add((1,2,3,"Hi")) # added tuple (immutable)
+
+print(collection,"\n")
+
+#collection.add([1,2,3]) #error will come
+
+# remove
+collection.remove(1)
+collection.remove(2)
+# collection.remove(7) # An error will be came.
+print(collection,"\n")
+
+#pop
+print(collection.pop()) #random value will be poped
+print(collection.pop(),"\n")
+
+
+# clear
+collection.clear() #Will give set() and length => 0.
+
+print(collection)
+print(len(collection),"\n")
+
+#=============================================================#
+set1 = {1,2,3}
+set2 = {2,3,4}
+
+#union
+print(set1.union(set2)) #{1,2,3,4}
+print(set1)
+print(set2,"\n")
+
+#intersection
+print(set1.intersection(set2)) # {2,3}
